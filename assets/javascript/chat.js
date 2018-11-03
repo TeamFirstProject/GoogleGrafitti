fedd0a4dc034043e8cfee314e5a5f30484bb684a
Dennis Tam <dennist82@Denniss-MacBook-Pro.local>
2018-11-03 12:47:39 -0700

    fix the switch chatroom bug.

--- assets/javascript/chat.js
+++ assets/javascript/chat.js
@@ -13,9 +13,9 @@
   var username = null;
   var input_message = $("#input_message");
   var currentChatRoom = "one";
-
   renderChatAppOptions("chatroom","chatroom");
-  renderChatAppOptions("members","members");
+ 
+  //renderChatAppOptions("members","members");
 //check localStorage get username from chatapp
 if(localStorage.chatappUsername == undefined){
     input_message.attr("placeholder","Enter your name.");
@@ -27,6 +27,7 @@ if(localStorage.chatappUsername == undefined){
 //update chatroom message when user select another chatroom.
 $("#chatroom").on("change",function(){
     currentChatRoom = $("#chatroom").val();
+    $("#chat_box").empty();
     database.ref("messages/"+currentChatRoom).once("value").then(function(snapshot){
         //this get multiple messages in one return.
         var messages = snapshot.val();
@@ -50,11 +51,13 @@ $("#chatroom").on("change",function(){
             $("#chat_box").append(newLine);
         }
     });
+    //attach listener.
+    
 })
 
 $("#submit").on("click",function(){
     event.preventDefault();
-    var chatroom = "messages/" + $("#chat_rooms_options").val();
+    var chatroom = "messages/" + $("#chatroom").val();
     var input = input_message.val();
     var today = new Date();
     var timestamp = today.getTime();
@@ -75,24 +78,45 @@ $("#submit").on("click",function(){
 });
 
 //Display messages from selected chatroom.
-database.ref(("messages/"+ currentChatRoom)).on("child_added",function(snapshot){
-    var messages = snapshot.val();
-    var timestamp = messages.timestamp;
-    var username = messages.username;
-    var message = messages.message;
-    var post = $("<span>");
-    var newLine = $("<br>");
-    if(username == localStorage.chatappUsername){
-        post.css("float","right");
-    }else{
-        post.css("float","left");
+database.ref(("messages/one")).on("child_added",function(snapshot){
+    if(currentChatRoom == "Dennis' Room"){
+        var messages = snapshot.val();
+        var timestamp = messages.timestamp;
+        var username = messages.username;
+        var message = messages.message;
+        var post = $("<span>");
+        var newLine = $("<br>");
+        if(username == localStorage.chatappUsername){
+            post.css("float","right");
+        }else{
+            post.css("float","left");
+        }
+        post.text(getTimeStamp(timestamp) + " " + username + " " + message);
+
+        $("#chat_box").append(post);
+        $("#chat_box").append(newLine);
     }
-    post.text(getTimeStamp(timestamp) + " " + username + " " + message);
-    //$("#chat_box").empty();
+});
 
-    $("#chat_box").append(post);
-    $("#chat_box").append(newLine);
-
+//Display messages from selected chatroom.
+database.ref(("messages/Dennis' Room")).on("child_added",function(snapshot){
+    if(currentChatRoom == "Dennis' Room"){
+        var messages = snapshot.val();
+        var timestamp = messages.timestamp;
+        var username = messages.username;
+        var message = messages.message;
+        var post = $("<span>");
+        var newLine = $("<br>");
+        if(username == localStorage.chatappUsername){
+            post.css("float","right");
+        }else{
+            post.css("float","left");
+        }
+        post.text(getTimeStamp(timestamp) + " " + username + " " + message);
+    
+        $("#chat_box").append(post);
+        $("#chat_box").append(newLine);
+    }
 });
 
 function createChatRoom(chatRoomName,x,y){
@@ -126,14 +150,38 @@ function renderChatAppOptions(group,class_name){
         var chatRoomIds = Object.keys(snapshot.val());
         class_name = "#"+class_name;
         $(class_name).empty();
-        chatRoomIds.forEach(function(chatRoomId){
-            var option = $("<option>",{value:chatRoomId, text:chatRoomId});
-            
+        for ( var i = 0; i < chatRoomIds.length;i++){
+            currentChatRoom = snapshot.val()[chatRoomIds[i]];        
+            var option = $("<option>",{value:chatRoomIds[i], text:chatRoomIds[i]});
+            addMarker({lat:currentChatRoom.x,lng:currentChatRoom.y})
             $(class_name).append(option);
-        })
+        }
         
     });
     currentChatRoom = $("#chatroom").val();
+    
 }
 
+//put messages from chatroom
+//Call initially seem to happen before select element for chatroom finish loading. 
+function getMessages(){
 
+    database.ref("messages/" + $("#chatroom").val()).once("value").then(function(snapshot){
+    
+        var messages = snapshot.val();
+        var timestamp = messages.timestamp;
+        var username = messages.username;
+        var message = messages.message;
+        var post = $("<span>");
+        var newLine = $("<br>");
+        if(username == localStorage.chatappUsername){
+            post.css("float","right");
+        }else{
+            post.css("float","left");
+        }
+        post.text(getTimeStamp(timestamp) + " " + username + " " + message);
+
+        $("#chat_box").append(post);
+        $("#chat_box").append(newLine);
+    });
+}
